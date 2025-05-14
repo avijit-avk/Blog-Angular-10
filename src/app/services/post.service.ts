3598171fcbab988be167d3c737331bdb8c72503f
Avijit Kumar <59834901+avijit-avk@users.noreply.github.com>
2025-05-14 14:02:00 +0530

    Update post.service.ts
    used render to deploy

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -8,7 +8,7 @@ import { Post } from '../Post';
 })
 export class PostService {
 
-  private apiUrl = "http://localhost:3000/posts";
+  private apiUrl = "https://json-server-blog-1.onrender.com";
 
   constructor(private http: HttpClient) { }
 
